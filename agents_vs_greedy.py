691fcb3e3082af783b22dec8adfb2d5f281c5870
sibieler <sibieler34@gmail.com>
2017-12-08 04:56:22 -0500

    Final?
    Fixed weights and agents and readme.

--- agents_vs_greedy.py
+++ agents_vs_greedy.py
@@ -14,7 +14,7 @@ def main():
 	p = Pool(20)
 	weights = [1.6793325000000001, 1.5973866250000002, 0.0815137, 0.045278125, 0.48488637500000004, 0.29009375000000004, 0.014928424999999999, 1.82054375]
 	gameBoard = board.BOARD()
-	agentTypes = ["Minimax", "Expectimax", "Greedy", "Greedy"]
+	agentTypes = ["Minimax", "Expectimax", "Greedy", "ProbGreedy"]
 	#expectimaxes = [EXPECTIMAX('Yellow','South', gameBoard,1,0,True,3,weights), EXPECTIMAX('Green','West', gameBoard,1,1,True,3,weights), EXPECTIMAX('Red','North', gameBoard,1,2,True,3,weights), EXPECTIMAX('Blue','East', gameBoard,1,3,True,3,weights)]
 	#minimaxes = [MINIMAX('Yellow','South', gameBoard,1,0,True,3,weights), MINIMAX('Green','West', gameBoard,1,1,True,3,weights), MINIMAX('Red','North', gameBoard,1,2,True,3,weights), MINIMAX('Blue','East', gameBoard,1,3,True,3,weights)]
 	agents = []
@@ -26,10 +26,10 @@ def main():
 				gameAgents.append(ags.MINIMAX(gameBoard.colors[j],gameBoard.directions[j], gameBoard, 1, j, True, 3, weights))
 			elif agentTypes[j] == "Expectimax":
 				gameAgents.append(ags.EXPECTIMAX(gameBoard.colors[j],gameBoard.directions[j], gameBoard, 1, j, True, 3, weights))
-			elif agentTypes[j] == "Greedy":
+			elif agentTypes[j] == "ProbGreedy":
 				gameAgents.append(ags.PROBGREEDY(gameBoard.colors[j],gameBoard.directions[j], gameBoard, j, weights))
 			else:
-				gameAgents.append(ags.RANDOM(gameBoard.colors[j],gameBoard.directions[j], gameBoard))
+				gameAgents.append(ags.GREEDY(gameBoard.colors[j],gameBoard.directions[j], gameBoard, j, weights))
 		gameAgents.append(copy.deepcopy(gameBoard))
 		gameAgents.append(False)
 		agents.append(gameAgents)
--- human_vs_agents.py
+++ human_vs_agents.py
@@ -14,7 +14,7 @@ The main function of the file. Runs a random game.
 '''
 def main():
 	gameBoard = board.BOARD()
-	humans = [ags.HUMAN('Yellow', 'South', gameBoard), ags.MINIMAX('Green','West', gameBoard, 1, 1, True, 3, [.8,1,.017,.025,.4,.25,.028,2]), ags.RANDOM('Red', 'North', gameBoard), ags.EXPECTIMAX('Blue','East', gameBoard, 1, 3, True, 3, [.8,1,.017,.025,.4,.25,.028,2]), gameBoard, True]
+	humans = [ags.HUMAN('Yellow', 'South', gameBoard), ags.MINIMAX('Green','West', gameBoard, 1, 1, True, 3, [1.6793325000000001, 1.5973866250000002, 0.0815137, 0.045278125, 0.48488637500000004, 0.29009375000000004, 0.014928424999999999, 1.82054375]), ags.PROBGREEDY('Red','North', gameBoard, 2, [1.6793325000000001, 1.5973866250000002, 0.0815137, 0.045278125, 0.48488637500000004, 0.29009375000000004, 0.014928424999999999, 1.82054375]), ags.EXPECTIMAX('Blue','East', gameBoard, 1, 3, True, 3, [1.6793325000000001, 1.5973866250000002, 0.0815137, 0.045278125, 0.48488637500000004, 0.29009375000000004, 0.014928424999999999, 1.82054375]), gameBoard, True]
 	play_game.playGame(humans)
 
 main()
